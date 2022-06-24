4f4a9904d16c9c0ff6c10500d71bc531d515bd1e
justdoanything <pure_yong@naver.com>
2022-06-24 22:35:50 +0900

    내용 추가

--- van/fdk/src/main/java/prj/yong/payment/approval/van/fdk/FdkApplication.java
+++ van/fdk/src/main/java/prj/yong/payment/approval/van/fdk/FdkApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// commit test
 public class FdkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FdkApplication.class, args);
+		
 	}
 
 }
