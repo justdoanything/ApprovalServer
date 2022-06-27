8e06eca57a350371410efeed3d35967ac28eea9a
justdoanything <pure_yong@naver.com>
2022-06-27 16:59:13 +0900

    불필요한 파일 삭제 및 이름 변경

--- kftc/src/main/java/prj/yong/payment/approval/van/kftc/controller/VanKftcController.java
+++ kftc/src/main/java/prj/yong/payment/approval/van/kftc/controller/VanKftcController.java
@@ -19,14 +19,14 @@ public class VanKftcController {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
     
     @Autowired
-    private VanKftcService vankftcService;
+    private VanKftcService vanKftcService;
 
     @ApiOperation(value = "kftc 전문 전송", httpMethod = "PUT", notes = "kftc 전문 전송")
     @PutMapping("/rest/van/kftc/v1.0")
-    public ResponseEntity<Object> sendApprovalMessageToVankftc(@RequestParam JsonObject request) throws Exception {
+    public ResponseEntity<Object> sendApprovalMessageToVanKftc(@RequestParam JsonObject request) throws Exception {
         
         LOGGER.debug("Calling url : /rest/van/kftc/v1.0\n- request : " + request.toString());
-        return vankftcService.sendApprovalMessageToVankftc(request);
+        return vanKftcService.sendApprovalMessageToVanKftc(request);
     }
     
 }
--- van/fdk/src/main/java/prj/yong/payment/approval/van/fdk/config/SwaggerConfig.java
+++
@@ -1,5 +0,0 @@
-package prj.yong.payment.approval.van.fdk.config;
-
-public class SwaggerConfig {
-    
-}
