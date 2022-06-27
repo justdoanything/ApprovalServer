73e872c02aa76d27be2f5f673878664116ce0539
justdoanything <pure_yong@naver.com>
2022-06-27 10:59:42 +0900

    Hystrix 추가 완료

--- fdk/src/main/java/prj/yong/payment/approval/van/fdk/FdkApplication.java
+++ fdk/src/main/java/prj/yong/payment/approval/van/fdk/FdkApplication.java
@@ -2,8 +2,10 @@ package prj.yong.payment.approval.van.fdk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class FdkApplication {
 
 	public static void main(String[] args) {
--- fdk/src/main/java/prj/yong/payment/approval/van/fdk/service/impl/VanFdkServiceImpl.java
+++ fdk/src/main/java/prj/yong/payment/approval/van/fdk/service/impl/VanFdkServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.JsonObject;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import prj.yong.payment.approval.van.fdk.service.VanFdkService;
 
@@ -11,8 +12,14 @@ import prj.yong.payment.approval.van.fdk.service.VanFdkService;
 public class VanFdkServiceImpl implements VanFdkService{
 
     @Override
+    @HystrixCommand(commandKey = "sendApprovalMessageToVanFdk", fallbackMethod = "fallbackSendApprovalMessageToVanFdk")
     public ResponseEntity<Object> sendApprovalMessageToVanFdk(JsonObject request) throws Exception {
         // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ResponseEntity<Object> fallbackSendApprovalMessageToVanFdk(JsonObject request, Throwable throwable){
+        // Gateway를 다시 호출해서 다른 VAN 서비스가 호출되게 해야함.
         return null;
     }
     
