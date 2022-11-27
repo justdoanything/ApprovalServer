2523ec087dff230755bb198e42c84952f67c0917
justdoanything <pure_yong@naver.com>
2022-11-27 15:40:01 +0900

    내용 수정

--- gateway/src/main/java/prj/yong/payment/approval/gateway/config/GlobalFilter.java
+++ gateway/src/main/java/prj/yong/payment/approval/gateway/config/GlobalFilter.java
@@ -1,46 +1,44 @@
 package prj.yong.payment.approval.gateway.config;
 
+import lombok.Data;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-
-import lombok.Data;
 import reactor.core.publisher.Mono;
 
 @Component
 public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
-  public GlobalFilter() {
-      super(Config.class);
-  }
-  
-  @Override
-  public GatewayFilter apply(Config config) {
-    return ((exchange, chain) -> {
-      ServerHttpRequest request = exchange.getRequest();
-      ServerHttpResponse response = exchange.getResponse();
-      
-      System.out.println("This is global filter : " + config.getBaseMessage());
-      if (config.isPreLogger()) {
-        // Pre Filter
-        System.out.println("This is pre global filter ");
+    public GlobalFilter() {
+        super(Config.class);
+    }
 
-      }
-      
-      return chain.filter(exchange).then(Mono.fromRunnable(()->{
-        if (config.isPostLogger()) {
-          // Post Filter
-          System.out.println("This is post global filter ");
-        }
-      }));
-    });
-  }
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
 
-  @Data
-  public static class Config {
-    private String baseMessage;
-    private boolean preLogger;
-    private boolean postLogger;
-  }
+            System.out.println("This is global filter : " + config.getBaseMessage());
+            if (config.isPreLogger()) {
+                // Pre Filter
+                System.out.println("This is pre global filter ");
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    // Post Filter
+                    System.out.println("This is post global filter ");
+                }
+            }));
+        });
+    }
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
 }
